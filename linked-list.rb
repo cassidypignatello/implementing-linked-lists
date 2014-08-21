9b5394bbfaecb2b65dc5ea7c30be8447858c15b7
cassidypignatello <cassidy.pignatello@gmail.com>
2014-08-21 15:59:20 -0400

    adds method for reversing linked list

--- linked-list.rb
+++ linked-list.rb
@@ -10,4 +10,22 @@ class Node
     return msg[0..-4] if node.nil?
     node_list(node.next, msg << "#{node.node} -> ")
   end
+
+  def self.reverse(node)
+    return node if node.next.nil?
+
+    head, swap, node.next = node.next, node, nil
+    link = head.next
+
+    while link != nil
+      head.next = swap
+      swap = head
+      head = link
+      link = link.next
+    end
+
+    head.next = swap
+    head
+    
+  end
 end
