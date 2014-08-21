d4a112d0722f9a2ae5a4e08a8abd722c8aa50d65
cassidypignatello <cassidy.pignatello@gmail.com>
2014-08-21 15:48:57 -0400

    adds node_list method

--- linked-list.rb
+++ linked-list.rb
@@ -4,4 +4,10 @@ class Node
   def initialize(node)
     @node = node
   end
+
+  def self.node_list(node, msg = nil)
+    msg ||= ""
+    return msg[0..-4] if node.nil?
+    node_list(node.next, msg << "#{node.node} -> ")
+  end
 end
